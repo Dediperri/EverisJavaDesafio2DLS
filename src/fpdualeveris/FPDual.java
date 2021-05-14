2d1ccedea5d3dc64d7349b8bd9c5f153692e79be
dlopezst <dlopezst@everis.com>
2021-05-14 12:10:05 +0200

    Version 1 - Desafio2

---
+++ src/fpdualeveris/FPDual.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package fpdualeveris;
+
+/**
+ * @author dlopezst
+ *
+ */
+public class FPDual {
+
+	/**
+	 * Metodo principal
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		operatorsChallenge();
+
+	}
+	
+	/**
+	 * Metodo privado donde se ejecutan todas las funciones de la clase pinball
+	 * */
+	private static void operatorsChallenge() {
+		
+	} 
+
+}
---
+++ src/operators/Pinball.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package operators;
+
+/**
+ * @author dlopezst
+ *
+ */
+public class Pinball {
+
+
+	/**
+	 * Método principal de la clase pinball que comienza el "juego"
+	 * */
+	public void launchBall() {
+		
+		//Generamos numeros de forma aleatoria
+		generateRandomNumbers(0, 0);
+		
+	}
+	
+	/**
+	 * Metodo que genera numeros de forma aleatoria en un rango especifico
+	 * @param int min
+	 * @param int max
+	 * */
+	private void generateRandomNumbers(int min, int max) {
+		
+	}
+	
+}
