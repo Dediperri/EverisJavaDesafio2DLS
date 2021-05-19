d97091f8203e655b70068710133e792a5a3308b8
dlopezst <dlopezst@everis.com>
2021-05-19 12:39:48 +0200

    Version 2 - Desafio 2

--- src/fpdualeveris/FPDual.java
+++ src/fpdualeveris/FPDual.java
@@ -3,27 +3,110 @@
  */
 package fpdualeveris;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import operators.Pinball;
+
 /**
  * @author dlopezst
  *
  */
 public class FPDual {
 
+	// Creo una instancia de scanner
+	private static Scanner input = new Scanner(System.in);
+
+	// Crea una lista con todas las partidas jugadas
+	private static List<Pinball> userScores = new ArrayList<>();
+
 	/**
 	 * Metodo principal
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		
+		StringBuilder sbuilder = new StringBuilder();
+        long inicio = System.currentTimeMillis();
+        for (int i=0; i<20000; i++) {
+            sbuilder.append("Elefante");
+        }
+        long fin = System.currentTimeMillis();
+        System.out.println("Tiempo del StringBuilder: " + (fin-inicio));      
 		
 		operatorsChallenge();
 
 	}
-	
+
 	/**
 	 * Metodo privado donde se ejecutan todas las funciones de la clase pinball
-	 * */
+	 */
 	private static void operatorsChallenge() {
-		
-	} 
+
+		String choice = "";
+
+		String menu = "--------------------" + "\n" + "- Jugar            -" + "\n" + "- Ver puntuaciones -" + "\n" + "- Salir            -" + "\n"
+		        + "--------------------";
+		;
+
+		while (!choice.equals("salir")) {
+
+			System.out.println(menu);
+
+			System.out.print("Qué deseas hacer? :");
+			try {
+				choice = input.nextLine();
+				choice = choice.toLowerCase();
+			} catch (IllegalArgumentException e) {
+				System.out.println("Valor introducido no válido: " + e);
+			}
+
+			switch (choice) {
+			case "jugar":
+				System.out.print("Introduce el nombre de usuario: ");
+				String player = "";
+				try {
+					player = input.nextLine();
+
+					// Creo instancias de la clase pinball y las añado a la lista
+					userScores.add(new Pinball(player));
+				} catch (IllegalArgumentException e) {
+					System.out.println("Valor introducido no válido: " + e);
+				}
+
+				break;
+			case "ver puntuaciones":
+				// Hago un foreach e imprimo por pantalla la puntuación final por jugador
+				printUsersScores();
+				break;
+
+			}
+		}
+
+		Pinball pinball = new Pinball();
+
+		if (pinball instanceof Object) {
+			System.out.println("Pinball es una instancia de objeto");
+		} else {
+			System.out.println("Pinball no es una instancia de objeto");
+		}
+
+	}
+
+	/**
+	 * Metodo estático que imprime los jugadores con sus scores totales
+	 */
+	private static void printUsersScores() {
+
+		for (Pinball pinball : userScores) {
+
+			System.out.println("Jugador: " + pinball.getPlayer() + " | Puntuación: " + pinball.getScore());
+
+		}
+
+	}
 
 }
--- src/operators/Pinball.java
+++ src/operators/Pinball.java
@@ -9,24 +9,196 @@ package operators;
  */
 public class Pinball {
 
+	StringBuilder stringBuilder = new StringBuilder();
+
+	/**Variable estatica pero privado que cuenta las veces que la bola ha rebotado*/
+	private static int numBounces = 0;
+
+	/** Variables privada que te cuenta el total de puntos*/
+	private int score = 0;
+
+	/**Variable privada que almacena el nombre del jugador*/
+	private String player = "";
+
+	/**
+	 * Constructor que no hace nada, simplemente para poder comprobar el instanceof
+	 */
+	public Pinball() {
+
+	}
+
+	/**
+	 * Constructor que inicia el juego, introduce el nick del jugador y te resetea el numero de rebotes a 0
+	 */
+	public Pinball(String player) {
+		this.setPlayer(player);
+		Pinball.numBounces = 0;
+		launchBall();
+	}
 
 	/**
 	 * Método principal de la clase pinball que comienza el "juego"
-	 * */
-	public void launchBall() {
-		
-		//Generamos numeros de forma aleatoria
-		generateRandomNumbers(0, 0);
-		
+	 */
+	private void launchBall() {
+
+		// Genero de forma aleatoria la cantidad de veces que se recorre el for, para que la bola continue rebotando
+		int gameTime = generateRandomNumbers(5, 20);
+		for (int i = 0; i < gameTime; i++) {
+
+			// Por cada vez que se ejecuta el metodo la bola rebota.
+			ballBounce();
+
+			try {
+
+				Thread.sleep(500);
+			} catch (Exception e) {
+
+				System.out.println("ERROR: " + e);
+			}
+		}
+
 	}
-	
+
+	/**
+	 * Metodo que se ejecuta cada vez que la bola rebota en alguna parte del pinball (que devuelve una puntuación)
+	 */
+
+	private void ballBounce() {
+
+		Pinball.numBounces += 1;
+
+		typesOfRewards(generateRandomNumbers(1, 3));
+
+	}
+
+	/**
+	 * Metodo que devuelve un tipo de objeto que sirve para determinar la cantidad de puntuación a sumar o restar
+	 */
+	private void typesOfRewards(int type) {
+
+		if (type == 1) {
+			if (addOrSubstract(generateRandomNumbers(1, 100))) {
+
+				updateScores(generateRandomNumbers(1, 25), type);
+
+			} else {
+
+				updateScores(generateRandomNumbers(-1, -25), type);
+
+			}
+		} else if (type == 2) {
+			if (addOrSubstract(generateRandomNumbers(0, 2))) {
+
+				updateScores(generateRandomNumbers(26, 100), type);
+
+			} else {
+
+				updateScores(generateRandomNumbers(-26, -100), type);
+
+			}
+
+		} else {
+			if (addOrSubstract(generateRandomNumbers(0, 2))) {
+
+				updateScores(generateRandomNumbers(101, 250), type);
+
+			} else {
+
+				updateScores(generateRandomNumbers(-101, -250), type);
+
+			}
+		}
+
+	}
+
+	/**
+	 * Metodo que actualiza la puntuacion e imprime por consola si se han restado o sumado puntos
+	 * 
+	 * @param
+	 */
+
+	private void updateScores(int score, int tier) {
+
+		if (numBounces != 0 && score > 0) {
+			score = score * Pinball.numBounces;
+		}
+
+		this.score += score;
+
+		if (score < 0) {
+			System.out.println(
+			        "No has rebotado bien!, se te resta: " + score + ", se ha reseteado el numero de rebotes seguidos!" + " Total: " + "(" + this.score + ")");
+			Pinball.numBounces = 0;
+		} else {
+			System.out.println("Rebotaste en un tier: " + tier + " conseguiste " + score + " puntos! " + "(" + Pinball.numBounces + ")" + " Total: " + "("
+			        + this.score + ")");
+		}
+
+	}
+
+	/**
+	 * Metodo que devuelve verdadero o falso dependiendo si lo introducido es mayor a 1 o menor
+	 * 
+	 * @param
+	 */
+	private Boolean addOrSubstract(int random) {
+
+		// Si devuelve positivo suma, si devuelve negativo resta
+		return (random > 0 && random < 80) ? true : false;
+	}
+
 	/**
 	 * Metodo que genera numeros de forma aleatoria en un rango especifico
-	 * @param int min
-	 * @param int max
-	 * */
-	private void generateRandomNumbers(int min, int max) {
-		
+	 * 
+	 * @param
+	 */
+	private int generateRandomNumbers(int min, int max) {
+
+		return (int) Math.floor(Math.random() * (max - min + 1) + min);
 	}
-	
+
+	/**
+	 * Metodo que devuelve el numero de rebotes
+	 * 
+	 * @return the numBounces
+	 */
+	public static int getNumBounces() {
+		return numBounces;
+	}
+
+	/**
+	 * Método que devuelve la puntuación
+	 * 
+	 * @return the score
+	 */
+	public int getScore() {
+		return score;
+	}
+
+	/**
+	 * Metodo que establece la puntuación
+	 * 
+	 * @param
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	/**
+	 * Metodo que devuelve el nombre del jugador
+	 * @return the player
+	 */
+	public String getPlayer() {
+		return player;
+	}
+
+	/**
+	 * Metodo que establece el nombre del jugador
+	 * @param player
+	 *            the player to set
+	 */
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+
 }
